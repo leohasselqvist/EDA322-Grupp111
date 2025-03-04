ec9a56c347bea943936046d010d4b4fb3e4ebaee
Leo Hasselqvist <leohas@chalmers.se>
2025-03-04 11:20:21 +0100

    lab3 done

--- rotateleft.vhd
+++ rotateleft.vhd
@@ -1,15 +1,17 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_misc.all;
+use IEEE.NUMERIC_STD.all;
 
 entity rotateleft is
-    generic( L : INTEGER := 1);
     port(   
     	    ain: in   std_logic_vector (7 downto 0);
+	    bin: in    std_logic_vector(2 downto 0);
     	    bout: out  std_logic_vector (7 downto 0)
    	);
 end rotateleft;
 
 architecture Dataflow of rotateleft is
 begin
-	bout <= ain(7-L downto 0) & ain(7 downto 7-L+1);
+	bout <= ain(7-to_integer(unsigned(bin)) downto 0) & ain(7 downto 7-to_integer(unsigned(bin))+1);
 end Dataflow;
--- rotatelefttestbench.vhd
+++ rotatelefttestbench.vhd
@@ -2,14 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity rotatelefttestbench is
-	generic (
-		L2 : integer := 4
-	);
 end rotatelefttestbench;
 
 architecture Behavior of rotatelefttestbench is
 signal ain1 : std_logic_vector(7 downto 0) := "10101010";
 signal ain2 : std_logic_vector(7 downto 0) := "00001111";
+signal bin1 : std_logic_vector(7 downto 0) := "00000001";
+signal bin2 : std_logic_vector(7 downto 0) := "00000100";
 signal bout1 : std_logic_vector(7 downto 0);
 signal bout2 : std_logic_vector(7 downto 0);
 signal boutCorr1 : std_logic_vector(7 downto 0) := "01010101";
@@ -20,14 +19,13 @@ begin
 	rol_1: entity work.rotateleft(Dataflow)
 			port map (
 				ain => ain1,
+				bin => bin1,
 				bout => bout1
 			);
 	rol_2: entity work.rotateleft(Dataflow)
-			generic map (
-				L => L2
-			)
 			port map (
 				ain => ain2,
+				bin => bin2,
 				bout => bout2
 			);
 	
